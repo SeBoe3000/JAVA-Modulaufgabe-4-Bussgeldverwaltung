623bbecdd0278938b9457cab5c9d5f1bd2bc6eef
SeBoe3000 <selina.boettger@gmx.de>
2024-12-21 15:48:48 +0100

    Tabelle erstellen und Beispieldaten in Einstiegsprogramm eingefügt

--- src/main/java/Bussgelder.java
+++ src/main/java/Bussgelder.java
@@ -1,7 +1,14 @@
+import Datenbank.BeispieldatenEinfuegen;
+import Datenbank.Tabellenerstellen;
 import Frontend.BussgelderGUI;
 
 public class Bussgelder {
     public static void main(String[] args) {
+        // Tabellen erstellen, falls noch nicht vorhanden
+        Tabellenerstellen.createTableall();
+        // Daten erstellen, falls noch nicht vorhanden
+        BeispieldatenEinfuegen.fillTableall();
+        // GUI aufrufen
         BussgelderGUI calc = new BussgelderGUI();
         calc.main();
     }
