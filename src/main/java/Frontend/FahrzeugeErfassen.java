6922d5f34f5515f92e4c8c9ed30158b7dd4f0498
SeBoe3000 <selina.boettger@gmx.de>
2024-12-22 12:55:15 +0100

    Felder bei Erfassung Fahrzeuge und Verstoss eingefügt

--- src/main/java/Frontend/FahrzeugeErfassen.java
+++ src/main/java/Frontend/FahrzeugeErfassen.java
@@ -5,10 +5,61 @@ import java.awt.*;
 
 public class FahrzeugeErfassen {
     private static final JFrame fahrzeuge = new JFrame("Fahrzeuge erfassen");
-
+    // Kennzeichen
+    EingabePanel kennzeichen = new EingabePanel("Kennzeichen: ");
+    // Modell
+    EingabePanel modell = new EingabePanel("Modell: ");
+    // Hersteller
+    EingabePanel hersteller = new EingabePanel("Hersteller: ");
+    // Motorleistung
+    EingabePanel motorleistung = new EingabePanel("Motorleistung: ");
+    // Transaktion-Buttons
+    JButton create_btn = new JButton("Erfassen");
+    JButton ok_btn = new JButton("OK");
+    JButton cancel_btn = new JButton("Abbrechen");
+    ButtonGroup transaction_group = new ButtonGroup();
 
     private void fahrzeuge() {
         JPanel panel = new JPanel();
+
+        // GridBagLayout
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        panel.setLayout(gridbag);
+
+        // Feld Kennzeichen hinzufügen
+        gbc.gridx = 0; // Spalte
+        gbc.gridy = 0; // Zeile
+        gbc.weightx = 0.1;
+        gbc.weighty = 0.1;
+        gbc.fill = GridBagConstraints.CENTER;
+        panel.add(kennzeichen, gbc);
+
+        // Feld Modell hinzufügen
+        gbc.gridy = 1; // Spalte
+        panel.add(modell, gbc);
+
+        // Feld Hersteller hinzufügen
+        gbc.gridy = 2; // Spalte
+        panel.add(hersteller, gbc);
+
+        // Feld Motorleistung hinzufügen
+        gbc.gridy = 3; // Spalte
+        panel.add(motorleistung, gbc);
+
+        // Transaction Buttons hinzufügen
+        transaction_group.add(create_btn);
+        transaction_group.add(ok_btn);
+        transaction_group.add(cancel_btn);
+
+        JPanel transaction_panel = new JPanel();
+        transaction_panel.setLayout(new BoxLayout(transaction_panel, BoxLayout.X_AXIS));
+        transaction_panel.add(create_btn);
+        transaction_panel.add(ok_btn);
+        transaction_panel.add(cancel_btn);
+
+        gbc.gridy = 4; // Spalte
+        panel.add(transaction_panel, gbc);
 
 
         // Panel dem Frame hinzufügen
--- src/main/java/Frontend/VerstossErfassen.java
+++ src/main/java/Frontend/VerstossErfassen.java
@@ -6,10 +6,61 @@ import java.awt.*;
 public class VerstossErfassen {
     private static final JFrame verstoss = new JFrame("Verstoss erfassen");
 
+    // ID
+    EingabePanel id = new EingabePanel("ID: ");
+    // Tageszeit
+    EingabePanel tageszeit = new EingabePanel("Tageszeit: ");
+    // VerstossID
+    EingabePanel verstossID = new EingabePanel("VerstossID: ");
+    // Fahrzeug
+    EingabePanel fahrzeug = new EingabePanel("Fahrzeug: ");
+    // Transaktion-Buttons
+    JButton create_btn = new JButton("Erfassen");
+    JButton ok_btn = new JButton("OK");
+    JButton cancel_btn = new JButton("Abbrechen");
+    ButtonGroup transaction_group = new ButtonGroup();
 
     private void verstoss() {
         JPanel panel = new JPanel();
 
+        // GridBagLayout
+        GridBagLayout gridbag = new GridBagLayout();
+        GridBagConstraints gbc = new GridBagConstraints();
+        panel.setLayout(gridbag);
+
+        // Feld Kennzeichen hinzufügen
+        gbc.gridx = 0; // Spalte
+        gbc.gridy = 0; // Zeile
+        gbc.weightx = 0.1;
+        gbc.weighty = 0.1;
+        gbc.fill = GridBagConstraints.CENTER;
+        panel.add(id, gbc);
+
+        // Feld Modell hinzufügen
+        gbc.gridy = 1; // Spalte
+        panel.add(tageszeit, gbc);
+
+        // Feld Hersteller hinzufügen
+        gbc.gridy = 2; // Spalte
+        panel.add(verstossID, gbc);
+
+        // Feld Motorleistung hinzufügen
+        gbc.gridy = 3; // Spalte
+        panel.add(fahrzeug, gbc);
+
+        // Transaction Buttons hinzufügen
+        transaction_group.add(create_btn);
+        transaction_group.add(ok_btn);
+        transaction_group.add(cancel_btn);
+
+        JPanel transaction_panel = new JPanel();
+        transaction_panel.setLayout(new BoxLayout(transaction_panel, BoxLayout.X_AXIS));
+        transaction_panel.add(create_btn);
+        transaction_panel.add(ok_btn);
+        transaction_panel.add(cancel_btn);
+
+        gbc.gridy = 4; // Spalte
+        panel.add(transaction_panel, gbc);
 
         // Panel dem Frame hinzufügen
         verstoss.add(panel);
